9d2ed48b85f3b954ac8eb48770b7a1a8f4620670
Jhansi Bata Reddy <jhansi.batareddy@agilisium.com>
2025-12-12 13:01:13 +0530

    implemented few features

--- frontend_app.py
+++ frontend_app.py
@@ -10,9 +10,10 @@ from typing import Optional
 
 # Config
 
-# API_BASE = os.getenv("API_BASE", "https://automobile-service-99o7.onrender.com/api")
+API_BASE = os.getenv("API_BASE", "https://automobile-service-99o7.onrender.com/api")
 
-API_BASE = os.getenv("API_BASE", "http://127.0.0.1:8000/api")
+
+# API_BASE = os.getenv("API_BASE", "http://127.0.0.1:8000/api")
 TIMEOUT = 10
 
 st.set_page_config(page_title="Automobile Service Manager", layout="wide")
